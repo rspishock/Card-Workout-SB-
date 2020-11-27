7e15c484c42af6b743316b39d10a9aa7b0ee3533
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2020-11-27 14:50:10 -0500

    Add outlets and actions as well as rules popup

--- Card Workout (SB)/CardSelectionVC.swift
+++ Card Workout (SB)/CardSelectionVC.swift
@@ -9,21 +9,24 @@ import UIKit
 
 class CardSelectionVC: UIViewController {
 
+    @IBOutlet var cardImageView: UIImageView!
+    @IBOutlet var buttons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        for button in buttons {
+            button.layer.cornerRadius = 8
+        }
+        
+    }       // viewDidLoad()
+    
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+    }       // stopButtonTapped
+    
+    @IBAction func restartButtonTapped(_ sender: UIButton) {
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func rulesButtonTapped(_ sender: UIButton) {
     }
-    */
-
 }
